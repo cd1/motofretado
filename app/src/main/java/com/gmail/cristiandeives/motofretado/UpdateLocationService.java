47fe840495391488eeca1c6444480161f413b9ff
Crístian Deives <cdeives@motorola.com>
2017-06-27 15:07:56 -0300

    Use Location API 11
    Signed-off-by: Crístian Deives <cdeives@motorola.com>

--- app/src/main/java/com/gmail/cristiandeives/motofretado/UpdateLocationService.java
+++ app/src/main/java/com/gmail/cristiandeives/motofretado/UpdateLocationService.java
@@ -11,12 +11,11 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.annotation.MainThread;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
@@ -25,21 +24,19 @@ import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.gmail.cristiandeives.motofretado.http.Bus;
 import com.gmail.cristiandeives.motofretado.http.ModelListener;
 import com.gmail.cristiandeives.motofretado.http.PatchBusRequest;
 import com.gmail.cristiandeives.motofretado.http.PatchBusResponseListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
-public class UpdateLocationService extends Service
-        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+public class UpdateLocationService extends Service {
     public static final String EXTRA_MESSENGER = "messenger";
     public static final String EXTRA_BUS_ID = "bus_id";
 
@@ -52,22 +49,38 @@ public class UpdateLocationService extends Service
 
     private Messenger mMessenger;
     private String mBusId;
-    private GoogleApiClient mGoogleApiClient;
     private RequestQueue mRequestQueue;
     private BroadcastReceiver mReceiver;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
 
     @Override
     @MainThread
     public void onCreate() {
         Log.v(TAG, "> onCreate()");
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        mRequestQueue = Volley.newRequestQueue(this);
-        mReceiver = new UpdateLocationBroadcastReceiver();
+        int permissionStatus = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            mLocationRequest = LocationRequest.create()
+                    .setInterval(UPDATE_INTERVAL)
+                    .setFastestInterval(FASTEST_UPDATE_INTERVAL)
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            mLocationCallback = new UpdateLocationCallback();
+
+            mRequestQueue = Volley.newRequestQueue(this);
+            mReceiver = new UpdateLocationBroadcastReceiver();
+        } else {
+            Message msg = Message.obtain(null, TrackBusPresenter.MyHandler.MSG_DISPLAY_TOAST,
+                    R.string.fine_location_permission_not_granted, 0);
+            try {
+                mMessenger.send(msg);
+            } catch (RemoteException ex) {
+                Log.e(TAG, "error sending message to display toast", ex);
+            }
+        }
 
         Log.v(TAG, "< onCreate()");
     }
@@ -83,65 +96,11 @@ public class UpdateLocationService extends Service
         mBusId = extras.getString(EXTRA_BUS_ID);
         Log.d(TAG, "extra \"" + EXTRA_BUS_ID + "\": " + mBusId);
 
-        Log.d(TAG, "opening connection to Google Play Services");
-        mGoogleApiClient.connect();
-
-        Log.d(TAG, "registering BroadcastReceiver");
-        registerReceiver(mReceiver, new IntentFilter(UpdateLocationBroadcastReceiver.STOP_SERVICE_ACTION));
-
-        Log.v(TAG, "< onStartCommand(intent=" + intent + ", flags=" + flags + ", startId=" + startId + ")");
-
-        return START_NOT_STICKY;
-    }
-
-    @Override
-    @MainThread
-    public IBinder onBind(Intent intent) {
-        // we're not a bound service, so return null here
-        Log.v(TAG, "onBind(intent=" + intent + ")");
-
-        return null;
-    }
-
-    @Override
-    @MainThread
-    public void onDestroy() {
-        Log.v(TAG, "> onDestroy()");
-
-        if (mGoogleApiClient.isConnected()) {
-            Log.d(TAG, "unsubscribing from location updates");
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            Log.d(TAG, "closing connection to Google Play Services");
-            mGoogleApiClient.disconnect();
-        }
-
-        Log.d(TAG, "unregistering BroadcastReceiver");
-        unregisterReceiver(mReceiver);
-
-        Message msg = Message.obtain(null, TrackBusPresenter.MyHandler.MSG_UPDATE_LOCATION_SERVICE_DISCONNECTED);
-        try {
-            mMessenger.send(msg);
-        } catch (RemoteException ex) {
-            Log.e(TAG, "error sending message to disconnect service", ex);
-        }
-
-        Log.v(TAG, "< onDestroy()");
-    }
-
-    @Override
-    @MainThread
-    public void onConnected(@Nullable Bundle connectionHint) {
-        Log.v(TAG, "> onConnected(connectionHint=" + connectionHint + ")");
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            LocationRequest locationRequest = new LocationRequest();
-            locationRequest.setInterval(UPDATE_INTERVAL);
-            locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL);
-            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            Log.d(TAG, "subscribing for location updates");
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
-                    locationRequest, this);
+        Log.d(TAG, "requesting location updates");
+        int permissionStatus = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper());
 
             Message msg = Message.obtain(null,
                     TrackBusPresenter.MyHandler.MSG_UPDATE_LOCATION_SERVICE_CONNECTED, mBusId);
@@ -170,8 +129,12 @@ public class UpdateLocationService extends Service
                     .build();
             Log.d(TAG, "marking the service as foreground / adding persistent notification");
             startForeground(NOTIFICATION_ID, notification);
+
+            Log.d(TAG, "registering BroadcastReceiver");
+            registerReceiver(mReceiver, new IntentFilter(UpdateLocationBroadcastReceiver.STOP_SERVICE_ACTION));
         } else {
-            Message msg = Message.obtain(null, TrackBusPresenter.MyHandler.MSG_DISPLAY_TOAST, R.string.fine_location_permission_not_granted, 0);
+            Message msg = Message.obtain(null, TrackBusPresenter.MyHandler.MSG_DISPLAY_TOAST,
+                    R.string.fine_location_permission_not_granted, 0);
             try {
                 mMessenger.send(msg);
             } catch (RemoteException ex) {
@@ -179,15 +142,31 @@ public class UpdateLocationService extends Service
             }
         }
 
-        Log.v(TAG, "< onConnected(connectionHint=" + connectionHint + ")");
+        Log.v(TAG, "< onStartCommand(intent=" + intent + ", flags=" + flags + ", startId=" + startId + ")");
+
+        return START_NOT_STICKY;
     }
 
     @Override
     @MainThread
-    public void onConnectionSuspended(int cause) {
-        Log.v(TAG, "> onConnectionSuspended(cause=" + cause + ")");
+    public IBinder onBind(Intent intent) {
+        // we're not a bound service, so return null here
+        Log.v(TAG, "onBind(intent=" + intent + ")");
 
-        Log.w(TAG, "Google Play Services connection was suspended!");
+        return null;
+    }
+
+    @Override
+    @MainThread
+    public void onDestroy() {
+        Log.v(TAG, "> onDestroy()");
+
+        Log.d(TAG, "unsubscribing from location updates");
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+
+        Log.d(TAG, "unregistering BroadcastReceiver");
+        unregisterReceiver(mReceiver);
+
         Message msg = Message.obtain(null, TrackBusPresenter.MyHandler.MSG_UPDATE_LOCATION_SERVICE_DISCONNECTED);
         try {
             mMessenger.send(msg);
@@ -195,42 +174,28 @@ public class UpdateLocationService extends Service
             Log.e(TAG, "error sending message to disconnect service", ex);
         }
 
-        Log.v(TAG, "< onConnectionSuspended(cause=" + cause + ")");
+        Log.v(TAG, "< onDestroy()");
     }
 
-    @Override
-    @MainThread
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        Log.v(TAG, "> onConnectionFailed(result=" + result + ")");
-
-        Log.e(TAG, "Google Play Services connection failed: " + result.getErrorMessage());
-        Message msg = Message.obtain(null, TrackBusPresenter.MyHandler.MSG_GMS_CONNECTION_FAILED);
-        try {
-            mMessenger.send(msg);
-        } catch (RemoteException ex) {
-            Log.e(TAG, "error sending message to display toast", ex);
-        }
-
-        stopSelf();
-
-        Log.v(TAG, "< onConnectionFailed(result=" + result + ")");
-    }
-
-    @Override
     @UiThread
-    public void onLocationChanged(Location location) {
-        Log.v(TAG, "> onLocationChanged(location=" + location + ")");
+    private class UpdateLocationCallback extends LocationCallback {
+        @Override
+        public void onLocationResult(LocationResult result) {
+            Log.v(TAG, "> onLocationResult(result=" + result + ")");
 
-        Bus bus = new Bus();
-        bus.id = mBusId;
-        bus.latitude = location.getLatitude();
-        bus.longitude = location.getLongitude();
+            Location lastLocation = result.getLastLocation();
 
-        Request<JSONObject> request = new PatchBusRequest(bus,
-                new PatchBusResponseListener(new PatchBusListener()));
-        mRequestQueue.add(request);
+            Bus bus = new Bus();
+            bus.id = mBusId;
+            bus.latitude = lastLocation.getLatitude();
+            bus.longitude = lastLocation.getLongitude();
 
-        Log.v(TAG, "< onLocationChanged(location=" + location + ")");
+            Request<JSONObject> request = new PatchBusRequest(bus,
+                    new PatchBusResponseListener(new PatchBusListener()));
+            mRequestQueue.add(request);
+
+            Log.v(TAG, "< onLocationResult(result=" + result + ")");
+        }
     }
 
     private class PatchBusListener implements ModelListener<Bus> {
